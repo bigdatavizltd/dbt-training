fffc4d10a6d46684534bbc513197a38c32ac5383
unutulmaz@hotmail.com <unutulmaz@hotmail.com>
2022-12-10 19:08:09 +0000

    s filenames

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,18 @@
-select * , 
-ordersellingprice - ordercostprice as orderprofit
+select 
+o.orderid ,
+o.orderdate,
+o.shipdate,
+o.shipmode, 
+o.ordersellingprice - o.ordercostprice as orderprofit ,
+---from raw customer
+c.customername,
+c.segment,
+c,country
+--- raw product
+p.category,
+p.productname,
+p.subcategory
 from 
-{{ ref('raw_orders') }}
+{{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+left join {{ ref('raw_product') }} as p
